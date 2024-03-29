f4d6d2a41164f05d594e362483d49806fd84bfc7
lenrek88 <lenrek88@yandex.ru>
2024-03-29 23:17:26 +0300

    12

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,7 @@ import Header from "../components/ui/header/header.jsx";
 import { FilmProvider } from "../components/context/context.js";
 import FilmCardInfo from "../components/ui/films/film_card_info.jsx";
 import Content from "../components/ui/films/content.jsx";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import GetToken from "../authorization/get_token.jsx";
 import PostToken from "../authorization/post_token.jsx";
 import getCookie from "../cookie/getCookie.js";
@@ -15,16 +15,18 @@ function App() {
     <div className="App">
       <FilmProvider>
         <Header />
-        {Token ? (
+        <HashRouter basename="/films">
+          {Token ? (
+            <Routes>
+              <Route path="/" element={<Content />} />
+              <Route path="/Film/:id" element={<FilmCardInfo />} />
+            </Routes>
+          ) : null}
           <Routes>
-            <Route path="/" element={<Content />} />
-            <Route path="/Film/:id" element={<FilmCardInfo />} />
+            <Route path="/Registration" element={<GetToken />} />
+            <Route path="/Authorisation" element={<PostToken />} />
           </Routes>
-        ) : null}
-        <Routes>
-          <Route path="/Registration" element={<GetToken />} />
-          <Route path="/Authorisation" element={<PostToken />} />
-        </Routes>
+        </HashRouter>
       </FilmProvider>
     </div>
   );
